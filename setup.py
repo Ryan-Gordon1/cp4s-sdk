3f17f1ccda3c39b30175afbcfc02da0dd66ad482
Ryan Gordon <Ryan.Gordon1@ibm.com>
2021-03-01 11:47:48 +0000

    Create initial setup.py
    Allows the project to be installed using pip which will then expose the command line tool.

---
+++ setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# (c) Copyright Ryan Gordon. 2021. All Rights Reserved.
+
+""" setup.py for cp4s-sdk Python Module """
+
+from setuptools import setup, find_packages
+setup(
+    name="cp4s_sdk",
+    version="1.0.0",
+    license="MIT",
+    packages=find_packages(),
+
+    # Installation Dependencies
+    setup_requires=[],
+
+    # Runtime Dependencies
+    install_requires=[],
+    # Add command line: cp4s-sdk
+    entry_points={
+        "console_scripts": ["cp4s-sdk=cp4s_sdk.sdk:main"]
+    },
+
+    # PyPI metadata
+    author="Ryan Gordon",
+    author_email="ryan.gordon1@ibm.com",
+    description="Python SDK for developing Extensions or Apps for any product under the Cloud Pak for Security Ecosystem",
+    url="https://github.com/Ryan-Gordon1/cp4s-sdk",
+    project_urls={
+        "IBM Community": "http://ibm.biz/soarcommunity",
+        "GitHub": "https://github.com/Ryan-Gordon1/cp4s-sdk"
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Security",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="ibm cloud pak cp4s cases qradar qapp circuits sdk resilient-sdk",
+)
