011ceef67bf351fa067a1cffe7cb4fefda477b1f
Ryan Gordon <Ryan.Gordon1@ibm.com>
2021-03-03 11:05:27 +0000

    Update sdk entrypoint to use soar sub parser

--- cp4s-sdk/sdk.py
+++ cp4s-sdk/sdk.py
@@ -4,6 +4,7 @@
 """ entrypoint for cp4s-sdk Python Module """
 
 # Python std lib deps
+from subparsers.soar_subparser import SoarSubParser
 from argparse import ArgumentParser, HelpFormatter
 import argparse
 
@@ -22,5 +23,30 @@ $cp4s-sdk cases -h
 or
 $cp4s-sdk qradar -h
 """
-
 app = argparse.ArgumentParser(description=DESCRIPTION)
+# Add the top level parser; this will be purely navigational
+root_sp = app.add_subparsers(dest='product')
+
+# Add the cases parser
+cases_subparser = SoarSubParser()
+cases_subparser.register_subparser(kwargs={
+    "parent_parser": root_sp
+})
+
+# TODO: Exploring looping instantiation
+# products = [CasesSubParser]
+# parser_dict = dict()
+# for p in products:
+#     p.__init__()
+#     p.register_subparser(p, kwargs={"parent_parser": root_sp})
+#     parser_dict.update({p.product: p})
+
+# After handling all the instantiation and setup. Parse the args so we can delagate the workload to the right product
+args = app.parse_args()
+
+# To minimize on code needed in this entrypoint
+# each product is defined as a class with common methods we can depend on here
+if args.product == 'soar':
+    cases_subparser.handle_command_invocation(product_args=args)
+else:
+    app.print_help()
