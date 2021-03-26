0c86e74ef88a47f115e1135becce0e8c9a20d524
Ryan-Gordon <Ryan.Gordon1@ibm.com>
2021-03-26 14:13:05 +0000

    Update SOAR parser with missings cmds

--- cp4s_sdk/subparsers/soar_subparser.py
+++ cp4s_sdk/subparsers/soar_subparser.py
@@ -3,8 +3,9 @@
 # (c) Copyright Ryan Gordon. 2021. All Rights Reserved.
 from .i_subparser import ISubParser
 from resilient_sdk.app import get_main_app_parser, get_main_app_sub_parser
+from resilient_sdk.util import sdk_helpers
 from resilient_sdk.util.sdk_argparse import SDKArgHelpFormatter
-from resilient_sdk.cmds import CmdDocgen, CmdCodegen, CmdClone, CmdExtract
+from resilient_sdk.cmds import CmdDocgen, CmdCodegen, CmdClone, CmdExtract, CmdExtPackage, CmdDev
 
 
 class SoarSubParser(ISubParser):
@@ -31,19 +32,24 @@ class SoarSubParser(ISubParser):
                 "No Parent Parser was found in kwargs, this is needed to ensure we can attach the {} commands".format(self.product))
 
         parent = kwargs.get("parent_parser")
+
+        # See if RES_SDK_DEV environment var is set
+        self.sdk_dev = sdk_helpers.is_env_var_set(sdk_helpers.ENV_VAR_DEV)
         # Top level product name
-        soar_parser = parent.add_parser(self.product, help="Commands for developing Cases Apps and Integrations",
+        self.parser = parent.add_parser(self.product, help="Commands for developing Cases Apps and Integrations",
                                         formatter_class=SDKArgHelpFormatter,
                                         conflict_handler='resolve')
 
         # Get sub_parser object which will attach the available commands for the product SDK
-        soar_sub_parser = get_main_app_sub_parser(soar_parser)
+        soar_sub_parser = get_main_app_sub_parser(self.parser)
         self.registered = True
         # By instantiating each command and passing the subparser we register each subcommand of the soar product
         self.cmd_codegen = CmdCodegen(soar_sub_parser)
         self.cmd_clone = CmdClone(soar_sub_parser)
         self.cmd_docgen = CmdDocgen(soar_sub_parser)
         self.cmd_extract = CmdExtract(soar_sub_parser)
+        self.cmd_ext_package = CmdExtPackage(soar_sub_parser)
+        self.cmd_dev = CmdDev(soar_sub_parser)
         return soar_sub_parser
 
     def is_registered(self, *args, **kwargs):
@@ -69,3 +75,5 @@ class SoarSubParser(ISubParser):
 
         elif self.sdk_dev and product_args.cmd == self.cmd_dev.CMD_NAME:
             self.cmd_dev.execute_command(product_args)
+        else:
+            self.parser.print_help()
