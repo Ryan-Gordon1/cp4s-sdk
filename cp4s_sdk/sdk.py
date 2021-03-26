0cf538291970acccd695cae1cc6e8975c9a2622c
Ryan-Gordon <Ryan.Gordon1@ibm.com>
2021-03-26 14:11:47 +0000

    Hook up the SDK to handle connector commands

--- cp4s_sdk/sdk.py
+++ cp4s_sdk/sdk.py
@@ -6,6 +6,7 @@
 # Python std lib deps
 from cp4s_sdk.subparsers.soar_subparser import SoarSubParser
 from cp4s_sdk.subparsers.car_subparser import CarSubParser
+from cp4s_sdk.subparsers.connector_subparser import ConnectorSubParser
 
 from argparse import ArgumentParser, HelpFormatter
 import argparse
@@ -20,38 +21,40 @@ the Cloud Pak.
 
 Run cp4s-sdk -h to see the commands
 And to see the command for each product try
-
+\n
 $cp4s-sdk cases -h
+\n
 or
+\n
 $cp4s-sdk qradar -h
 """
-app = argparse.ArgumentParser(description=DESCRIPTION)
-# Add the top level parser; this will be purely navigational
-root_sp = app.add_subparsers(dest='product')
+def main():
+    app = argparse.ArgumentParser(description=DESCRIPTION)
+    # Add the top level parser; this will be purely navigational
+    root_sp = app.add_subparsers(dest='product')
 
-# Add the cases parser
-cases_subparser = SoarSubParser()
-cases_subparser.register_subparser(parent_parser=root_sp)
+    # Add the cases parser
+    cases_subparser = SoarSubParser()
+    cases_subparser.register_subparser(parent_parser=root_sp)
+    connector_subparser = ConnectorSubParser()
+    connector_subparser.register_subparser(parent_parser=root_sp)
 
-car_subparser = CarSubParser()
-car_subparser.register_subparser(parent_parser=root_sp)
+    # TODO: Exploring looping instantiation
+    # products = [CasesSubParser]
+    # parser_dict = dict()
+    # for p in products:
+    #     p.__init__()
+    #     p.register_subparser(p, kwargs={"parent_parser": root_sp})
+    #     parser_dict.update({p.product: p})
 
-# TODO: Exploring looping instantiation
-# products = [CasesSubParser]
-# parser_dict = dict()
-# for p in products:
-#     p.__init__()
-#     p.register_subparser(p, kwargs={"parent_parser": root_sp})
-#     parser_dict.update({p.product: p})
+    # After handling all the instantiation and setup. Parse the args so we can delagate the workload to the right product
+    args = app.parse_args()
 
-# After handling all the instantiation and setup. Parse the args so we can delagate the workload to the right product
-args = app.parse_args()
-
-# To minimize on code needed in this entrypoint
-# each product is defined as a class with common methods we can depend on here
-if args.product == 'soar':
-    cases_subparser.handle_command_invocation(product_args=args)
-if args.product == 'car':
-    car_subparser.handle_command_invocation(product_args=args)
-else:
-    app.print_help()
+    # To minimize on code needed in this entrypoint
+    # each product is defined as a class with common methods we can depend on here
+    if args.product == 'soar':
+        cases_subparser.handle_command_invocation(product_args=args)
+    elif args.product == 'connector':
+        connector_subparser.handle_command_invocation(product_args=args)
+    else:
+        app.print_help()
